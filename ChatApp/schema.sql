5a5c7d712856aba75ad663dda565d4b098210da1
Jasmin Mohamed <jasmingiire1@gmail.com>
2021-06-27 22:27:55 +0300

    Updated db schema

--- ChatApp/schema.sql
+++ ChatApp/schema.sql
@@ -1,45 +1,74 @@
-CREATE TABLE "users" (
+CREATE TABLE "Users" (
   "id" SERIAL PRIMARY KEY,
-  "firstname" varchar unique not null,
-  "lastname" varchar not null,
-  "username" varchar not null,
-  "salted_passwd" varchar not null,
-  "admin" boolean not null,
-  "created_at" timestamp not null default now()
+  "firstname" varchar(100),
+  "salted_passwd" varchar,
+  "created_at" timestamp,
+  "is_admin" boolean
 );
 
-CREATE TABLE "channels" (
+CREATE TABLE "Channel" (
   "id" SERIAL PRIMARY KEY,
-  "topic" varchar not null,
-  "owner_id" int not null,
-  "created_at" timestamp not null default now()
+  "title" varchar(100),
+  "description" text,
+  "created_at" timestamp,
+  "is_secret" boolean
 );
 
-CREATE TABLE "threads" (
-  "id" SERIAL PRIMARY KEY,
-  "channel_id" int not null,
-  "owner_id" int not null,
-  "title" varchar not null,
-  "created_at" timestamp not null default now()
+CREATE TABLE "Secret_Channel_Users" (
+  "user_id" int,
+  "channel_id" int,
+  PRIMARY KEY ("user_id", "channel_id")
 );
 
-CREATE TABLE "messages" (
+CREATE TABLE "Channel_Rating" (
+  "user_id" int,
+  "channel_id" int,
+  "rating" int,
+  PRIMARY KEY ("user_id", "channel_id")
+);
+
+CREATE TABLE "Thread" (
   "id" SERIAL PRIMARY KEY,
-  "thread_id" int not null,
-  "sender_id" int not null,
+  "channel_id" int,
+  "owner_id" int,
+  "title" varchar(100),
+  "created_at" timestamp
+);
+
+CREATE TABLE "Message" (
+  "id" SERIAL PRIMARY KEY,
+  "sender_id" int,
+  "thread_id" int,
   "reply_id" int,
-  "content" varchar not null,
-  "sent" timestamp not null default now()
+  "content" varchar(200),
+  "send_at" timestamp
 );
 
-ALTER TABLE "channels" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id");
+CREATE TABLE "Likes" (
+  "user_id" int,
+  "message_id" int,
+  PRIMARY KEY ("user_id", "message_id")
+);
 
-ALTER TABLE "threads" ADD FOREIGN KEY ("channel_id") REFERENCES "channels" ("id");
+ALTER TABLE "Secret_Channel_Users" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("id");
 
-ALTER TABLE "threads" ADD FOREIGN KEY ("owner_id") REFERENCES "users" ("id");
+ALTER TABLE "Secret_Channel_Users" ADD FOREIGN KEY ("channel_id") REFERENCES "Channel" ("id");
 
-ALTER TABLE "messages" ADD FOREIGN KEY ("thread_id") REFERENCES "threads" ("id");
+ALTER TABLE "Channel_Rating" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("id");
 
-ALTER TABLE "messages" ADD FOREIGN KEY ("sender_id") REFERENCES "users" ("id");
+ALTER TABLE "Channel_Rating" ADD FOREIGN KEY ("channel_id") REFERENCES "Channel" ("id");
 
-ALTER TABLE "messages" ADD FOREIGN KEY ("reply_id") REFERENCES "messages" ("id");
+ALTER TABLE "Thread" ADD FOREIGN KEY ("channel_id") REFERENCES "Channel" ("id");
+
+ALTER TABLE "Thread" ADD FOREIGN KEY ("owner_id") REFERENCES "Users" ("id");
+
+ALTER TABLE "Message" ADD FOREIGN KEY ("sender_id") REFERENCES "Users" ("id");
+
+ALTER TABLE "Message" ADD FOREIGN KEY ("thread_id") REFERENCES "Thread" ("id");
+
+ALTER TABLE "Message" ADD FOREIGN KEY ("reply_id") REFERENCES "Message" ("id");
+
+ALTER TABLE "Likes" ADD FOREIGN KEY ("user_id") REFERENCES "Users" ("id");
+
+ALTER TABLE "Likes" ADD FOREIGN KEY ("message_id") REFERENCES "Message" ("id");
+
